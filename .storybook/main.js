64a072eaf7adf657a827711ab6845414ff70736c
Alexandru Fiodor <alexandrufiodor95@gmail.com>
2021-05-16 00:11:37 +0300

    Storybook, snapshot

---
+++ .storybook/main.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-actions",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        rule: {
+          test: [/\.stories\.tsx?$/],
+        },
+        loaderOptions: {
+          prettierConfig: {
+            printWidth: 80, singleQuote: false,
+            options: {parser: 'typescript'}
+          },
+        },
+      },
+    }
+  ],
+}
---
+++ src/EditableSpan.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {action} from "@storybook/addon-actions";
+import {EditableSpan} from "./EditableSpan";
+
+export default {
+    title: 'EditableSpan Component',
+    component: EditableSpan
+}
+
+const onChange = action('Change span')
+const changeTaskTitle = action('Title changed')
+
+export const EditableSpanBaseExample = (props: any) => {
+    return <EditableSpan value={''} onChange={onChange}/>
+}
